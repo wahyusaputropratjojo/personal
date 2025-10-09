96b0495d55fde0c35fe69228fa48daaece01b888
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-10-09 18:40:44 +0800

    feat(section): create high contrast backgrounds section for colors page

---
+++ app/design-system/colors/_sections/high-contrast-backgrounds.tsx
@@ -0,0 +1,39 @@
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+
+export function HighContrastBackgroundsSection() {
+  return (
+    <section className="space-y-3">
+      <div className="space-y-1">
+        <h1 className="text-heading-24 lg:text-heading-32">
+          High Contrast Backgrounds
+        </h1>
+        <p className="text-pretty text-copy-14 text-gray-900 lg:text-copy-16">
+          These two colors are designed for high contrast UI component
+          backgrounds.
+        </p>
+      </div>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell className="w-6">
+              <div className="size-6 rounded-full border border-gray-400 bg-gray-700" />
+            </TableCell>
+            <TableCell>Color 7</TableCell>
+            <TableCell className="text-right text-gray-900">
+              High contrast background
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="w-6">
+              <div className="size-6 rounded-full border border-gray-400 bg-gray-800" />
+            </TableCell>
+            <TableCell>Color 8</TableCell>
+            <TableCell className="text-right text-gray-900">
+              Hover high contrast background
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </section>
+  );
+}
