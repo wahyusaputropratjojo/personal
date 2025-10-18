93d4492437cadeafef052b15ffd46977fcaa13a7
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-10-18 17:30:31 +0800

    feat(component): create label component

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import { Label as LabelPrimitive } from "radix-ui";
-import type { ComponentProps } from "react";
-import { cn } from "@/lib/utils/cn";
+import {
+  Label as LabelPrimitive,
+  type LabelProps as LabelPrimitiveProps,
+} from "react-aria-components";
 
-type LabelProps = ComponentProps<typeof LabelPrimitive.Root>;
+type LabelProps = Omit<LabelPrimitiveProps, "className">;
 
-export function Label({ className, ...props }: LabelProps) {
-  return (
-    <LabelPrimitive.Root
-      className={cn(
-        "flex select-none items-center gap-2 font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
-        className
-      )}
-      data-slot="label"
-      {...props}
-    />
-  );
+export function Label({ ...props }: LabelProps) {
+  return <LabelPrimitive data-slot="label" {...props} />;
 }
