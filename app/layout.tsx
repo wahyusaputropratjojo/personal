9c5ad8e5205e52fff7acc0bf8bfb2c8ecdd6b18f
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-12-02 19:25:28 +0800

    feat(layout): apply theme provider by next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,28 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import type { ReactNode } from "react";
 import { geistMono, geistSans } from "@/styles/fonts/geist";
 import "./style.css";
 
 export const metadata: Metadata = {
-  description: "Personal Site",
-  title: "Wahyu Saputro Pratjojo",
+	description: "Personal Site",
+	title: "Wahyu Saputro Pratjojo",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: ReactNode;
+	children: ReactNode;
 }>) {
-  return (
-    <html
-      className={`${geistSans.variable} ${geistMono.variable} selection:bg-gray-1000 selection:text-background-100`}
-      data-theme="dark"
-      lang="en"
-    >
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html
+			className={`${geistSans.variable} ${geistMono.variable}`}
+			lang="en"
+			suppressHydrationWarning
+		>
+			<body>
+				<ThemeProvider>{children}</ThemeProvider>
+			</body>
+		</html>
+	);
 }
