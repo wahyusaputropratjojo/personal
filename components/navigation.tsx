88ea46d85ec0c3354270c82e5336ac46dc68f902
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-12-04 18:51:41 +0800

    feat(component): create navigation component

---
+++ components/navigation.tsx
@@ -0,0 +1,12 @@
+export function Navigation() {
+	return (
+		<nav className="flex flex-col p-4 sm:flex-row sm:gap-8">
+			<p className="font-medium text-xl tracking-tighter">
+				Wahyu Saputro Pratjojo
+			</p>
+			<p className="font-medium text-neutral-11 text-xl tracking-tighter">
+				Web Developer
+			</p>
+		</nav>
+	);
+}
