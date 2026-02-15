2065348b0c2d95fec3db97fa7e5fb7582f9f5eb3
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-15 18:10:04 +0800

    feat(component): create portrait component

---
+++ src/lib/components/portrait.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { animate } from "motion";
+  import type { HTMLImgAttributes } from "svelte/elements";
+
+  $effect(() => {
+    animate(
+      "#portrait",
+      {
+        clipPath: "inset(0% 0% 0% 0%)",
+      },
+      {
+        duration: 1.5,
+        ease: "anticipate",
+      },
+    );
+  });
+
+  let props: HTMLImgAttributes = $props();
+</script>
+
+<img id="portrait" style:clip-path="inset(0% 0% 100% 0%)" {...props} />
