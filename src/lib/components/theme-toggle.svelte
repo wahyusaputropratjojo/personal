8cdb14acdebce716d0fbec0be909da50ceb7f7c4
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-16 20:20:49 +0800

    feat(component): create theme toggle component

---
+++ src/lib/components/theme-toggle.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { MoonIcon, SunIcon } from "@lucide/svelte";
+  import { mode, setMode } from "mode-watcher";
+  import Button from "$lib/components/button.svelte";
+
+  function toggleMode() {
+    const newTheme = mode.current === "light" ? "dark" : "light";
+
+    if (!document.startViewTransition) {
+      setMode(newTheme);
+      return;
+    }
+
+    document.startViewTransition(() => {
+      setMode(newTheme);
+    });
+  }
+</script>
+
+<Button size="small" intent="secondary" onclick={toggleMode}>
+  {#if mode.current === "light"}
+    <SunIcon />
+  {:else}
+    <MoonIcon />
+  {/if}
+</Button>
+
+<style>
+  ::view-transition-old(root) {
+    animation: none;
+  }
+
+  ::view-transition-new(root) {
+    animation: wipe-up 1000ms cubic-bezier(0.77, 0, 0.18, 1);
+  }
+
+  @keyframes wipe-up {
+    from {
+      clip-path: inset(100% 0% 0% 0%);
+    }
+    to {
+      clip-path: inset(0% 0% 0% 0%);
+    }
+  }
+</style>
