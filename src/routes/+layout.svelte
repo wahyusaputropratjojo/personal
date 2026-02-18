3f7a75ba5f375f17774775091e5ad7738f4bbc84
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-18 21:16:49 +0800

    feat(layout): update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { ModeWatcher } from "mode-watcher";
   import favicon from "$lib/assets/favicon.svg";
+  import Header from "$lib/components/header.svelte";
+  import TabsBar from "$lib/components/tabs-bar.svelte";
   import "./global.css";
 
   const { children } = $props();
@@ -9,4 +12,24 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
+<ModeWatcher track={false} />
+<Header />
+<TabsBar class="layout__tabs-bar" />
 {@render children()}
+
+<style>
+  :global {
+    .layout__tabs-bar {
+      position: fixed;
+      bottom: 0;
+      z-index: 100;
+      padding-block: 1rem;
+      padding-inline: 1rem;
+      background: linear-gradient(to top, var(--color-neutral-1), transparent);
+
+      @media (width >= 48rem) {
+        display: none;
+      }
+    }
+  }
+</style>
