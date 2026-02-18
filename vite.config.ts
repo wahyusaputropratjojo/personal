96a69c9571f3d079f56d0f4c4ec5566b57220bf3
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-18 20:45:06 +0800

    chore: update vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    enhancedImages(),
     sveltekit(),
   ],
 });
