4e5edf413e4394d7d885b3c245d52cee26f55886
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-23 20:51:20 +0800

    feat(component): create label component

---
+++ src/lib/components/ui/label.svelte
@@ -0,0 +1,53 @@
+<script lang="ts" module>
+  import { cva, type VariantProps } from "class-variance-authority";
+
+  export type LabelProps = Label.RootProps & VariantProps<typeof labelVariants>;
+
+  export const labelVariants = cva("label", {
+    variants: {
+      size: {
+        large: "label--large",
+        medium: "label--medium",
+        small: "label--small",
+      },
+    },
+    defaultVariants: {
+      size: "medium",
+    },
+  });
+</script>
+
+<script lang="ts">
+  import { Label } from "bits-ui";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    size,
+    ...props
+  }: LabelProps = $props();
+</script>
+
+<Label.Root bind:ref class={labelVariants({ size, className })} {...props} />
+
+<style>
+  :global {
+    .label {
+      font-weight: 500;
+      line-height: 1.375;
+      letter-spacing: normal;
+    }
+
+    .label--large {
+      font-size: var(--label-large);
+    }
+
+    .label--medium {
+      font-size: var(--label-medium);
+    }
+
+    .label--small {
+      font-size: var(--label-small);
+    }
+  }
+</style>
