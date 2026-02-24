56266152f80e7782fc61a4d4c470623e5b3dd3b7
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-24 19:54:40 +0800

    feat(component): update theme toggle component

--- src/lib/components/theme-toggle.svelte
+++ src/lib/components/theme-toggle.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { MoonIcon, SunIcon } from "@lucide/svelte";
   import { mode, setMode } from "mode-watcher";
-  import Button from "$lib/components/button.svelte";
+  import Button from "$lib/components/ui/button.svelte";
 
   function toggleMode() {
     const newTheme = mode.current === "light" ? "dark" : "light";
