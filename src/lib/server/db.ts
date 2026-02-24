5ac71fad81d2ee3f9bf30a1c1446b13dfa812f8c
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-24 19:55:54 +0800

    feat(db): create resuable mongodb client instance

---
+++ src/lib/server/db.ts
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+import { MONGODB_URI } from "$env/static/private";
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable.");
+}
+
+let instanceMongoClient: MongoClient;
+
+if (import.meta.env.DEV) {
+  type Global = typeof globalThis & {
+    _instanceMongoClient?: MongoClient;
+  };
+
+  const global: Global = globalThis;
+
+  if (!global._instanceMongoClient) {
+    global._instanceMongoClient = new MongoClient(MONGODB_URI);
+  }
+
+  instanceMongoClient = global._instanceMongoClient;
+} else {
+  instanceMongoClient = new MongoClient(MONGODB_URI);
+}
+
+export const client = instanceMongoClient;
