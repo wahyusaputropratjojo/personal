9530ca1e630af444a44de056ea1fc2892206058c
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-24 19:52:04 +0800

    feat(component): update navigation component

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
-  import { NavigationMenu } from "bits-ui";
+  import * as NavigationMenu from "$lib/components/ui/navigation-menu";
   import { page } from "$app/state";
 
   const routes = [
     {
+      id: "aa47feff-1eb6-4ce7-b681-050000e97d3a",
       href: "/",
       title: "Home",
     },
     {
+      id: "b88ba4ab-151d-4adf-b395-0afd77b3b11e",
       href: "/about",
       title: "About",
     },
     {
+      id: "004f5861-72f0-4b52-95dd-e354b2daa2b3",
       href: "/contact",
       title: "Contact",
     },
@@ -20,99 +23,14 @@
   let props = $props();
 </script>
 
-<NavigationMenu.Root
-  {...props}
-  class={["navigation-menu", props.class]}
-  value={page.url.pathname}
->
-  <NavigationMenu.List class="navigation-menu__list">
-    {#each routes as { href, title }}
-      <NavigationMenu.Item class="navigation-menu__item">
-        <NavigationMenu.Link
-          active={page.url.pathname === href}
-          class="navigation-menu__trigger"
-          {href}
-        >
+<NavigationMenu.Root value={page.url.pathname} {...props}>
+  <NavigationMenu.List>
+    {#each routes as { id, href, title } (id)}
+      <NavigationMenu.Item>
+        <NavigationMenu.Link active={page.url.pathname === href} {href}>
           {title}
         </NavigationMenu.Link>
       </NavigationMenu.Item>
     {/each}
   </NavigationMenu.List>
 </NavigationMenu.Root>
-
-<style>
-  :global {
-    .navigation-menu {
-      min-width: max-content;
-    }
-
-    .navigation-menu__list {
-      position: relative;
-      display: flex;
-      gap: 0.25rem;
-      align-items: center;
-      padding: 0;
-      margin: 0;
-      list-style: none;
-    }
-
-    .navigation-menu__item {
-      position: relative;
-    }
-
-    .navigation-menu__trigger {
-      --link-border-radius: calc(
-        var(--popup-border-radius) - var(--popup-padding)
-      );
-
-      box-sizing: border-box;
-      display: inline flex;
-      gap: 0.25rem;
-      align-items: center;
-      width: 100%;
-      min-height: 2rem;
-      padding: 0.2rem 0.5rem;
-      font-size: var(--label-medium);
-      font-weight: var(--weight-medium);
-      line-height: var(--leading-normal);
-      color: var(--color-neutral-11);
-      letter-spacing: var(--tracking-normal);
-      text-decoration: none;
-      user-select: none;
-      outline-color: transparent;
-      border-radius: var(--link-border-radius, calc(infinity * 1px));
-      transition: color;
-      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-      transition-duration: 150ms;
-
-      & svg {
-        width: 0.75rem;
-        height: 0.75rem;
-      }
-
-      &:disabled {
-        pointer-events: none;
-        opacity: 50%;
-      }
-
-      &:hover {
-        color: var(--color-neutral-12);
-        background-color: var(--color-neutral-4);
-      }
-
-      &:focus-visible {
-        color: var(--color-neutral-12);
-        outline-width: 3px;
-        outline-style: solid;
-        outline-color: var(--color-primary-8);
-        outline-offset: 1px;
-        background-color: var(--color-neutral-4);
-      }
-
-      &[data-active] {
-        color: var(--color-primary-12);
-        background-color: var(--color-primary-5);
-      }
-    }
-  }
-</style>
