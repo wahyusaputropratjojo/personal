dabdb188d4f7e2cff98e2325c41ab3969bf2ffcd
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-25 20:33:15 +0800

    feat(content): create separator component

---
+++ src/lib/components/ui/separator.svelte
@@ -0,0 +1,39 @@
+<script lang="ts" module>
+  export type SeparatorProps = Separator.RootProps;
+</script>
+
+<script lang="ts">
+  import { Separator } from "bits-ui";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    ...props
+  }: SeparatorProps = $props();
+</script>
+
+<Separator.Root
+  bind:ref
+  data-slot="separator"
+  class={["separator", className]}
+  {...props}
+/>
+
+<style>
+  :global {
+    .separator {
+      flex-shrink: 0;
+      background-color: var(--color-neutral-6, gray);
+
+      &[data-orientation="horizontal"] {
+        min-width: 100%;
+        height: 1px;
+      }
+
+      &[data-orientation="vertical"] {
+        width: 1px;
+        min-height: 100%;
+      }
+    }
+  }
+</style>
