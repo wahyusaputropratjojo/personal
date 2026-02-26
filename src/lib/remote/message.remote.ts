bf8859f1c09bdbd526c001ae7a1dac1b712d3d6e
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-02-26 20:50:40 +0800

    feat(remote): create remote function for messages

---
+++ src/lib/remote/message.remote.ts
@@ -0,0 +1,77 @@
+import { z } from "zod";
+import { form, query, command } from "$app/server";
+import { db } from "$lib/server/database";
+import { MessageSchema } from "$lib/schemas/message-schema";
+import { ObjectId } from "mongodb";
+
+interface Message extends z.infer<typeof MessageSchema> {
+  _id: string;
+  status: "active" | "archived";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const getMessages = query(async (): Promise<Message[]> => {
+  const messages = await db
+    .collection<Message>("messages")
+    .find()
+    .sort({ createdAt: -1 })
+    .limit(10)
+    .toArray();
+
+  return messages.map((message) => ({
+    ...message,
+    _id: message._id.toString(),
+  }));
+});
+
+export const createMessage = form(MessageSchema, async (message) => {
+  await db.collection<Omit<Message, "_id">>("messages").insertOne({
+    ...message,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    status: "active",
+  });
+
+  await getMessages().refresh();
+});
+
+export const deleteMessage = command(z.string(), async (id) => {
+  const _id = new ObjectId(id);
+
+  await db.collection("messages").findOneAndDelete({
+    _id,
+  });
+
+  await getMessages().refresh();
+});
+
+export const archiveMessage = command(z.string(), async (id) => {
+  const _id = new ObjectId(id);
+
+  await db.collection("messages").findOneAndUpdate(
+    { _id },
+    {
+      $set: {
+        status: "archived",
+      },
+    },
+  );
+
+  await getMessages().refresh();
+});
+
+export const activeMessage = command(z.string(), async (id) => {
+  const _id = new ObjectId(id);
+
+  await db.collection("messages").findOneAndUpdate(
+    { _id },
+    {
+      $set: {
+        status: "active",
+      },
+    },
+  );
+
+  await getMessages().refresh();
+});
