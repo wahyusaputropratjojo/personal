f99d13f374956c37b821a6b84e3f3d6bdf8567ee
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-03-01 18:54:42 +0800

    feat(component): create aspect ratio component

---
+++ src/lib/components/ui/aspect-ratio.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { AspectRatio } from "bits-ui";
+  import type { ComponentProps } from "svelte";
+
+  type Props = ComponentProps<typeof AspectRatio.Root>;
+
+  let { ref = $bindable(null), ...props }: Props = $props();
+</script>
+
+<AspectRatio.Root bind:ref data-slot="aspect-ratio" {...props} />
