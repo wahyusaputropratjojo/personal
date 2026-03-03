a422ac37f46a75ac31f25d87f40fb3f0342e68af
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2026-03-03 17:56:42 +0800

    feat(component): create index export for dialog component

---
+++ src/lib/components/ui/dialog/index.ts
@@ -0,0 +1,10 @@
+export { default as DialogClose } from "./dialog-close.svelte";
+export { default as DialogContent } from "./dialog-content.svelte";
+export { default as DialogDescription } from "./dialog-description.svelte";
+export { default as DialogFooter } from "./dialog-footer.svelte";
+export { default as DialogHeader } from "./dialog-header.svelte";
+export { default as DialogOverlay } from "./dialog-overlay.svelte";
+export { default as DialogPortal } from "./dialog-portal.svelte";
+export { default as Dialog } from "./dialog-root.svelte";
+export { default as DialogTitle } from "./dialog-title.svelte";
+export { default as DialogTrigger } from "./dialog-trigger.svelte";
